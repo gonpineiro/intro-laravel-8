2db82e4f1d7bb9cd861d1ce5fbcded4f9b60bae9
Gonzalo <gon.pineiro@gmail.com>
2021-02-24 11:19:45 -0300

    [add] - PageController

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.web')
 
 @section('content')
+Detalle del curso
 <div class="text-center">
     <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos cursos</h1>
     <h2 class="text-xl text-gray-600">Fórmate online como profesional en tenología</h2>
-    <h3 class="text-lg text-gray-600">70% de los graduados duplican sus ingresos</h3>
 </div>
 <livewire:course-list />
 @endsection
